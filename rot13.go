0ce54213b62c19cece527021bb595f1d50421766
artjomnazarov <kooduser@pupil.kood.tech>
2024-06-01 14:23:01 +0300

    root13

---
+++ rot13.go
@@ -0,0 +1,23 @@
+package survivalists
+
+func encrypt_rot13(s string) string {
+	var str string
+	for _, i := range s {
+		if i >= 'a' && i <= 'z' {
+			i = i + 13
+			for i > 'z' {
+				i = i - 26
+			}
+			str += string(i)
+		} else if i >= 'A' && i <= 'Z' {
+			i = i + 13
+			for i > 'Z' {
+				i = i - 26
+			}
+			str += string(i)
+		} else {
+			str += string(i)
+		}
+	}
+	return str
+}
