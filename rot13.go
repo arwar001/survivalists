20df779d7def340e51aa8a46eb3286d1af5b5220
artjomnazarov <kooduser@pupil.kood.tech>
2024-06-01 14:27:21 +0300

    root13

--- rot13.go
+++ rot13.go
@@ -21,3 +21,25 @@ func encrypt_rot13(s string) string {
 	}
 	return str
 }
+
+func decrypt_rot13(s string) string {
+	var str string
+	for _, i := range s {
+		if i >= 'a' && i <= 'z' {
+			i = i - 13
+			for i > 'z' {
+				i = i + 26
+			}
+			str += string(i)
+		} else if i >= 'A' && i <= 'Z' {
+			i = i - 13
+			for i > 'Z' {
+				i = i + 26
+			}
+			str += string(i)
+		} else {
+			str += string(i)
+		}
+	}
+	return str
+}
