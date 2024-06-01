f35fa739e03f366e7b1f078ecd7662a64a3cc90d
sokradd <ssokradd@gmail.com>
2024-06-01 14:02:51 +0300

    decrypt_reverse

---
+++ decrypt_reverse.go
@@ -0,0 +1,13 @@
+package survivalists
+
+func decrypt_reverse(s string) string {
+	result := []rune(s)
+	for i, r := range result {
+		if r >= 'a' && r <= 'z' {
+			result[i] = 'z' + 'a' - r
+		} else if r >= 'A' && r <= 'Z' {
+			result[i] = 'Z' + 'A' - r
+		}
+	}
+	return string(result)
+}
